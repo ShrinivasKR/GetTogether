3629b3978834968245e55481dc371bbaa3fe028e
Taran <TaranChristensen@gmail.com>
2015-05-11 20:25:35 -0700

    Test-push

--- server.js
+++ server.js
@@ -28,3 +28,5 @@ require('./app/routes')(app); // pass our application into our routes
 app.listen(port);	
 console.log('Magic happens on port ' + port); 			// shoutout to the user
 exports = module.exports = app; 						// expose app
+
+//Quick test-push comment
