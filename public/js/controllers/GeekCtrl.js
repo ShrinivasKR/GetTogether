e5d38daa2aac6e8c23f7e7a0267e055834b4af17
Taran <TaranChristensen@gmail.com>
2015-05-17 20:19:23 -0700

    Working simple MapsAPI and geolocation, with code-based radar places search. Temporarily on the Geek page.

--- public/js/controllers/GeekCtrl.js
+++ public/js/controllers/GeekCtrl.js
@@ -2,37 +2,33 @@ angular.module('GeekCtrl', []).controller('GeekController', function($scope) {
 
 	$scope.tagline = 'MapsAPI stuff put here temporarily';	
 
+    // ALL the maps stuff!
 	var map;
 
-	function initialize()
-	{
+	function initialize() {
 	    var mapOptions = { zoom: 15 };
 	    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 	    // Try HTML5 geolocation
-	    if (navigator.geolocation)
-	    {
-	        navigator.geolocation.getCurrentPosition(function (position)
-	        {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(function (position) {
 	            var pos = new google.maps.LatLng(position.coords.latitude,
                                                  position.coords.longitude);
 
 	            var infowindow = new google.maps.InfoWindow(
                 {
-	                map: map,
-	                position: pos,
-	                content: 'Location found using HTML5.'
-	            });
+                    map: map,
+                    position: pos,
+                    content: 'Location found using HTML5.'
+                });
 
 	            map.setCenter(pos);
 	        },
-            function ()
-	        {
-	            handleNoGeolocation(true);
-	        });
+            function () {
+                handleNoGeolocation(true);
+            });
 	    }
-	    else
-	    {
+	    else {
 	        // Browser doesn't support Geolocation
 	        handleNoGeolocation(false);
 	    }
@@ -43,76 +39,65 @@ angular.module('GeekCtrl', []).controller('GeekController', function($scope) {
 	    google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
 	}
 
-	function handleNoGeolocation(errorFlag)
-	{
-	    if (errorFlag)
-	    {
+	function handleNoGeolocation(errorFlag) {
+	    if (errorFlag) {
 	        var content = 'Error: The Geolocation service failed.';
 	    }
-	    else
-	    {
+	    else {
 	        var content = 'Error: Your browser doesn\'t support geolocation.';
 	    }
 
 	    var options =
         {
-	        map: map,
-	        position: new google.maps.LatLng(60, 105),
-	        content: content
-	    };
+            map: map,
+            position: new google.maps.LatLng(60, 105),
+            content: content
+        };
 
 	    var infowindow = new google.maps.InfoWindow(options);
 	    map.setCenter(options.position);
 	}
 
 
-	function performSearch()
-	{
+	function performSearch() {
 	    var request =
         {
-	        bounds: map.getBounds(),
-	        keyword: 'coffee'
-	    };
+            bounds: map.getBounds(),
+            keyword: 'coffee'
+        };
 	    service.radarSearch(request, callback);
 	}
 
-	function callback(results, status)
-	{
-	    if (status != google.maps.places.PlacesServiceStatus.OK)
-	    {
+	function callback(results, status) {
+	    if (status != google.maps.places.PlacesServiceStatus.OK) {
 	        alert(status);
 	        return;
 	    }
-	    for (var i = 0, result; result = results[i]; i++)
-	    {
+	    for (var i = 0, result; result = results[i]; i++) {
 	        createMarker(result);
 	    }
 	}
 
-	function createMarker(place)
-	{
+	function createMarker(place) {
 	    var marker = new google.maps.Marker(
         {
-	        map: map,
-	        position: place.geometry.location,
-	        icon:
+            map: map,
+            position: place.geometry.location,
+            icon:
             {
-	            // Star
-	            path: 'M 0,-24 6,-7 24,-7 10,4 15,21 0,11 -15,21 -10,4 -24,-7 -6,-7 z',
-	            fillColor: '#ffff00',
-	            fillOpacity: 1,
-	            scale: 1 / 4,
-	            strokeColor: '#bd8d2c',
-	            strokeWeight: 1
-	        }
-	    });
+                // Star
+                path: 'M 0,-24 6,-7 24,-7 10,4 15,21 0,11 -15,21 -10,4 -24,-7 -6,-7 z',
+                fillColor: '#ffff00',
+                fillOpacity: 1,
+                scale: 1 / 4,
+                strokeColor: '#bd8d2c',
+                strokeWeight: 1
+            }
+        });
 
-	    google.maps.event.addListener(marker, 'click', function ()
-	    {
-	        service.getDetails(place, function (result, status)
-	        {
-	            if (status != google.maps.places.PlacesServiceStatus.OK)
-	            {
+	    google.maps.event.addListener(marker, 'click', function () {
+	        service.getDetails(place, function (result, status) {
+	            if (status != google.maps.places.PlacesServiceStatus.OK) {
 	                alert(status);
 	                return;
 	            }
