3b24054584aadc7efaa9eedc44e371affdb0c27a
ShrinivasKR <shrinivas.k.ramanath@gmail.com>
2015-05-20 00:00:09 -0700

    Added Buttons for the events

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,10 @@
-var app = angular.module('StarterApp', ['ngMaterial', 'ngMdIcons', 'ngRoute', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService', 'LoginCtrl', 'GroupsCtrl', 'EventCtrl', 'ProfileCtrl', 'SettingsCtrl']);
+var app = angular.module('StarterApp', ['ngMaterial', 'ngMdIcons', 'ngRoute', 'ui.bootstrap', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService', 'LoginCtrl', 'GroupsCtrl', 'EventCtrl', 'ProfileCtrl', 'SettingsCtrl']);
 
 app.controller('AppCtrl', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog', function ($scope, $mdBottomSheet, $mdSidenav, $mdDialog, $location, $log) {
-
+    $scope.checkModel = {
+    middle: true,
+    right: false
+  };
     $scope.toggleSidenav = function (menuId) {
         $mdSidenav(menuId).toggle();
     };
