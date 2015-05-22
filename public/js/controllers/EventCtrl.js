6b0f4551634bba1252db7002e86920b71eac08fb
ShrinivasKR <shrinivas.k.ramanath@gmail.com>
2015-05-21 23:55:01 -0700

    Added some time picker elements to the time section in events page. don't know if it will be used though

--- public/js/controllers/EventCtrl.js
+++ public/js/controllers/EventCtrl.js
@@ -1,9 +1,39 @@
-angular.module('EventCtrl', ['ngMaterial', 'ngMessages']).controller('EventController', function ($scope) {
+//, 'rgkevin.datetimeRangePicker', 'vr.directives.slider', 'ngTouch'], [require('angular-touch')]
+angular.module('EventCtrl', ['ngMaterial', 'ngMessages', 'ui.bootstrap']).controller('EventController', function ($scope, $log) {
 
         $scope.tagline = 'The square root of life is pi!';
         $scope.user = {
             username: 'Enter Username',
             password: 'Enter Password',
+        };
+        $scope.mytime = new Date();
+
+        $scope.hstep = 1;
+        $scope.mstep = 15;
+
+        $scope.options = {
+            hstep: [1, 2, 3],
+            mstep: [1, 5, 10, 15, 25, 30]
+        };
+
+        $scope.ismeridian = true;
+        $scope.toggleMode = function () {
+            $scope.ismeridian = !$scope.ismeridian;
+        };
+
+        $scope.update = function () {
+            var d = new Date();
+            d.setHours(14);
+            d.setMinutes(0);
+            $scope.mytime = d;
+        };
+
+        $scope.changed = function () {
+            $log.log('Time changed to: ' + $scope.mytime);
+        };
+
+        $scope.clear = function () {
+            $scope.mytime = null;
         };
     })
     .config(function ($mdThemingProvider) {
