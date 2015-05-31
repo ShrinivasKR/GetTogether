b30e43710711d8b9e6b1a168be33e14a8e4dea62
Taran <TaranChristensen@gmail.com>
2015-05-31 14:33:09 -0700

    Changed Profile page to use angular, rather than raw javascript

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -1,10 +1,27 @@
 angular.module('ProfileCtrl', ['ngMaterial', 'ngMessages']).controller('ProfileController', function ($scope) {
-
-        $scope.tagline = 'The square root of life is pi!';
         $scope.user = {
             username: 'Enter Username',
             password: 'Enter Password',
         };
+
+        $scope.Times = [];
+        $scope.TimesLength = 105;
+
+        for (i = 0; i <= $scope.TimesLength; i++) {
+            $scope.Times[i] = false;
+        }
+
+        $scope.setColor = function (btn, num) {
+            var property = document.getElementById(btn);
+            if ($scope.Times[num] == true) {
+                property.style.backgroundColor = "#FFFFFF"
+                $scope.Times[num] = false;
+            } else {
+                property.style.backgroundColor = "#76FF03"
+                $scope.Times[num] = true;
+            }
+        }
+        
     })
     .config(function ($mdThemingProvider) {
 
