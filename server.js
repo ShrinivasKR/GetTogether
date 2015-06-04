d4ef920249895904c768218ef3a42234407687b7
Duc Ngo <dngo@live.com>
2015-06-04 03:17:49 -0700

    Clean up

--- server.js
+++ server.js
@@ -16,37 +16,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 // configuration ===========================================
-	
-// config files
-/*
-var passport = require('passport');
-	var Auth0Strategy = require('passport-auth0');
 
-	var strategy = new Auth0Strategy({
-    	domain:       'gettogetherusers.auth0.com',
-    	clientID:     'DvVb0iJIFk7b9xr9hDVk6IyGflK4CI1C',
-    	clientSecret: 'Ybyy2VZFRqHKpbbn8s3UqBCavI8FPsLp84gC-uD8CIe-j2yhd0lxrGua56OudHPy',
-    	callbackURL:  '/callback'
-  }, function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  });
-
-passport.use(strategy);
-
-// This is not a best practice, but we want to keep things simple for now
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-module.exports = strategy;
-*/
 app.use(cookieParser());
 app.use(session({ secret: 'shhhhhhhhh' }));
 
--- setup-passport.js
+++ setup-passport.js
@@ -7,7 +7,7 @@ var strategy = new Auth0Strategy({
     clientSecret: 'R3JX57AUrGMIy2WxJHN_7DtKWoqX1hX_cJhfN2bVml_YTpaHsw5evqUZ3Te6p24w',
     callbackURL:  '/callback'
   }, function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // accessToken is the token to call Auth0 API (not needed in most cases)
     // extraParams.id_token has the JSON Web Token
     // profile has all the information from the user
     return done(null, profile);
